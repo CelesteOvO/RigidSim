eaf852b0448bf7d41d46ce68feeee93502368ae7
Celeste <1227657366@qq.com>
2023-11-03 17:43:57 +0800

    Initial

---
+++ src/contact/Contact.cpp
@@ -0,0 +1,115 @@
+#include "contact/Contact.h"
+#include "rigidbody/RigidBody.h"
+
+Contact::Contact() : p(), n(), t1(), t2(), mu(0.4f),
+    body0(nullptr), body1(nullptr), k(1e6f), b(1e5f), index(-1), pene(0.0f)
+{
+
+}
+
+Contact::Contact(RigidBody* _body0, RigidBody* _body1, const Eigen::Vector3f& _p, const Eigen::Vector3f& _n, float _pene) :
+    p(_p), n(_n), t1(), t2(), mu(0.4f), pene(_pene), body0(_body0), body1(_body1), k(1e6f), b(1e5f), index(-1)
+{
+    J0.setZero(3, 6);
+    J1.setZero(3, 6);
+    J0Minv.setZero(3, 6);
+    J1Minv.setZero(3, 6);
+    lambda.setZero(3);
+    phi.setZero(3);
+    phi(0) = _pene;
+
+    body0->contacts.push_back(this);
+    body1->contacts.push_back(this);
+}
+
+Contact::~Contact()
+{
+
+}
+
+void Contact::computeContactFrame()
+{
+    // Compute the contact frame, which consists of an orthonormal
+    //  bases formed the vector n, t1, and t2
+    // 
+    //  The first bases direction is given by the normal, n.
+    //  Use it to compute the other two directions.
+
+    // TODO Compute first tangent direction t1
+    //
+
+
+    // TODO Compute second tangent direction t2.
+    //
+    /*Eigen::Vector3f dir = Eigen::Vector3f(1,0,0);
+    t1 = dir - (dir.dot(n))*n;
+    if( t1.norm() < 1e-5f )
+    {
+        // Fail-safe: use axis-aligned direction (0,0,-1)
+        t1 = -n.cross( Eigen::Vector3f(0,0,-1) );
+    }
+    t1.normalize();
+
+    // Compute second tangent direction.
+    //
+    t2 = n.cross(t1);
+    t2.normalize();*/
+
+    Eigen::Vector3f tangent1 = Eigen::Vector3f::Zero();
+    Eigen::Vector3f tangent2 = Eigen::Vector3f::Zero();
+    Eigen::Vector3f n = this->n;
+
+    if (n.x() == 0 && n.y() == 0) {
+        tangent1 = Eigen::Vector3f(1, 0, 0);
+    } else {
+        tangent1 = Eigen::Vector3f(n.y(), -n.x(), 0);
+    }
+    tangent1.normalize();
+    tangent2 = n.cross(tangent1);
+    tangent2.normalize();
+
+    this->t1 = tangent1;
+    this->t2 = tangent2;
+}
+
+void Contact::computeJacobian()
+{
+    // TODO Compute the Jacobians J0 and J1 
+    // for body0 and body1, respectively.
+    const Eigen::Vector3f r0 = p - body0->x;
+    const Eigen::Vector3f r1 = p - body1->x;
+
+    J0.setZero(3, 6);
+    J1.setZero(3, 6);
+    J0Minv.setZero(3, 6);
+    J1Minv.setZero(3, 6);
+    lambda.setZero(3);
+    phi.setZero(3);
+    phi(0) = pene;
+
+    // normal row (non-interpenetration)
+    J0.block(0, 0, 1, 3) = n.transpose();
+    J0.block(0, 3, 1, 3) = r0.cross(n).transpose();
+    J1.block(0, 0, 1, 3) = -n.transpose();
+    J1.block(0, 3, 1, 3) = -r1.cross(n).transpose();
+    // tangent 1 (friction)
+    J0.block(1, 0, 1, 3) = t1.transpose();
+    J0.block(1, 3, 1, 3) = r0.cross(t1).transpose();
+    J1.block(1, 0, 1, 3) = -t1.transpose();
+    J1.block(1, 3, 1, 3) = -r1.cross(t1).transpose();
+    // tangent 2 (friction)
+    J0.block(2, 0, 1, 3) = t2.transpose();
+    J0.block(2, 3, 1, 3) = r0.cross(t2).transpose();
+    J1.block(2, 0, 1, 3) = -t2.transpose();
+    J1.block(2, 3, 1, 3) = -r1.cross(t2).transpose();
+
+    // Compute the J M^-1 blocks for each body. The code is provided.
+    // 
+    // However, together with the contact Jacobians J0 and J1, these will
+    //   be used by the solver to assemble the blocked LCP matrices.
+    //
+    J0Minv.block(0,0,3,3) = (1.0f/body0->mass) * J0.block(0, 0, 3, 3);
+    J0Minv.block(0,3,3,3) = J0.block(0, 3, 3, 3) * body0->Iinv;
+    J1Minv.block(0,0,3,3) = (1.0f/body1->mass) * J1.block(0, 0, 3, 3);
+    J1Minv.block(0,3,3,3) = J1.block(0, 3, 3, 3) * body1->Iinv;
+}
---
+++ src/solvers/SolverBoxPGS.cpp
@@ -0,0 +1,168 @@
+#include "solvers/SolverBoxPGS.h"
+
+#include "contact/Contact.h"
+#include "rigidbody/RigidBody.h"
+#include "rigidbody/RigidBodySystem.h"
+
+#include <Eigen/Dense>
+
+namespace
+{
+    static inline void multAndSub(const JBlock& G, const Eigen::Vector3f& x, const Eigen::Vector3f& y, const float a, Eigen::VectorXf& b)
+    {
+        b -= a * G.col(0) * x(0);
+        b -= a * G.col(1) * x(1);
+        b -= a * G.col(2) * x(2);
+        b -= a * G.col(3) * y(0);
+        b -= a * G.col(4) * y(1);
+        b -= a * G.col(5) * y(2);
+    }
+
+    static inline void buildRHS(Contact* c, float h, Eigen::VectorXf& b)
+    {
+        const float gamma = h * c->k / (h * c->k + c->b);       // error reduction parameter
+        b = -gamma * c->phi / h;
+
+        multAndSub(c->J0, c->body0->xdot, c->body0->omega, 1.0f, b);
+        multAndSub(c->J1, c->body1->xdot, c->body1->omega, 1.0f, b);
+
+        if( !c->body0->fixed )
+        {
+            multAndSub(c->J0Minv, c->body0->f, c->body0->tau, h, b);
+        }
+        if( !c->body1->fixed )
+        {
+            multAndSub(c->J1Minv, c->body1->f, c->body1->tau, h, b);
+        }
+    }
+
+    static inline void accumulateCoupledContacts(Contact* c, const JBlock& JMinv, RigidBody* body, Eigen::VectorXf& b)
+    {
+        if( body->fixed )
+            return;
+
+        for(Contact* cc : body->contacts)
+        {
+            if( cc != c )
+            {
+                if( body == cc->body0 )
+                    b -= JMinv * (cc->J0.transpose() * cc->lambda);
+                else
+                    b -= JMinv * (cc->J1.transpose() * cc->lambda);
+            }
+        }
+    }
+
+    static inline void solveContact(const Eigen::Matrix3f& A, const Eigen::VectorXf& b, Eigen::VectorXf& x, const float mu)
+    {
+        // Normal impulse is projected to [0, inf]
+        //
+        x(0) = std::max(0.0f, (b(0) - A(0,1) * x(1) - A(0,2) * x(2) ) / A(0,0) );
+
+        // Next, friction impulses are projected to [-mu * x(0), mu * x(1)]
+        //
+        x(1) = std::max(-mu*x(0), std::min(mu*x(0), ( b(1) - A(1,0) * x(0) - A(1,2) * x(2) ) / A(1,1) ));
+        x(2) = std::max(-mu*x(0), std::min(mu*x(0), ( b(2) - A(2,0) * x(0) - A(2,1) * x(1) ) / A(2,2) ));
+    }
+}
+
+SolverBoxPGS::SolverBoxPGS(RigidBodySystem* _rigidBodySystem) : Solver(_rigidBodySystem)
+{
+
+}
+
+void SolverBoxPGS::solve(float h)
+{
+    // TODO Implement a PGS method that solves for the
+    //      contact constraint forces in @a rigidBodySystem. 
+    //      Assume a boxed LCP formulation.
+    //
+    std::vector<Contact*>& contacts = m_rigidBodySystem->getContacts();
+    const int numContacts = contacts.size();
+
+    // Build array of 3x3 diagonal matrices, one for each contact.
+    // 
+    std::vector<Eigen::Matrix3f> Acontactii;
+    if( numContacts > 0 )
+    {
+
+        // TODO Compute the right-hand side vector : b = -gamma*phi/h - J*vel - dt*JMinvJT*force
+        Acontactii.resize(numContacts);
+        for(int i = 0; i < numContacts; ++i)
+        {
+            Contact* c = contacts[i];
+            const float eps = 1.0f / (h * h * c->k + h * c->b);    // constraint force mixing
+
+            // Compute the diagonal term : Aii = J0*Minv0*J0^T + J1*Minv1*J1^T
+            //
+            Acontactii[i].setZero(3,3);
+            Acontactii[i](0,0) += eps;
+
+            if( !c->body0->fixed )
+            {
+                Acontactii[i] += c->J0Minv * c->J0.transpose();
+            }
+            if( !c->body1->fixed )
+            {
+                Acontactii[i] += c->J1Minv * c->J1.transpose();
+            }
+        }
+
+        std::vector<Eigen::VectorXf> b;
+        b.resize(numContacts);
+
+        for(int i = 0; i < numContacts; ++i)
+        {
+            Contact* c = contacts[i];
+            buildRHS(c, h, b[i]);
+            c->lambda.setZero();
+        }
+
+        // PGS main loop.
+        // There is no convergence test here. Simply stop after @a maxIter iterations.
+        //
+        for(int iter = 0; iter < m_maxIter; ++iter)
+        {
+            // TODO For each contact, compute an updated value of contacts[i]->lambda
+            //      using matrix-free pseudo-code provided in the course appendix.
+            //
+            for(int i = 0; i < numContacts; ++i)
+            {
+                // TODO initialize current solution as x = b(i)
+                // 
+
+                // TODO Loop over all other contacts involving c->body0
+                //      and accumulate : x -= (J0*Minv0*Jother^T) * lambda_other
+                //
+
+
+                // TODO Loop over all other contacts involving c->body1
+                //      and accumulate : x -= (J0*Minv0*Jother^T) * lambda_other
+                //
+
+
+                // TODO Compute the diagonal term : Aii = J0*Minv0*J0^T + J1*Minv1*J1^T
+                //
+
+
+                // TODO Update lambda by solving the sub-problem : Aii * contacts[i].lambda = x
+                //      For non-interpenetration constraints, lambda is non-negative and should be clamped
+                //      to the range (0... infinity).
+                //      Otherwise friction constraints should be clamped to the range (-mu*lambda_n, mu*lambda_n)
+                //      where lambda_n is the impulse of the corresponding non-interpentration constraint.
+                //
+
+                Contact* c = contacts[i];
+
+                // Initialize current solution as x = b[i]
+                Eigen::VectorXf x = b[i];
+
+                accumulateCoupledContacts(c, c->J0Minv, c->body0, x);
+                accumulateCoupledContacts(c, c->J1Minv, c->body1, x);
+                solveContact(Acontactii[i], x, c->lambda, c->mu);
+
+            }
+        }
+    }
+}
+
